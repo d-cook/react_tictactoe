1b965ee56e06eb46f83a1baf84bd33fddc2a9d50
Dan Cook <dcook0819@gmail.com>
2021-07-04 10:01:48 -0700

    Split into components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import "./styles.css";
 
+const isWin = (pieces, x, y) => {
+  const piece = pieces[y][x];
+  const L = pieces[y][(x + 2) % 3];
+  const R = pieces[y][(x + 1) % 3];
+  const U = pieces[(y + 2) % 3][x];
+  const D = pieces[(y + 1) % 3][x];
+  const UL = pieces[(y + 2) % 3][(x + 2) % 3];
+  const UR = pieces[(y + 2) % 3][(x + 1) % 3];
+  const DL = pieces[(y + 1) % 3][(x + 2) % 3];
+  const DR = pieces[(y + 1) % 3][(x + 1) % 3];
+  const isOnUpDiag = x - y === 0;
+  const isOnDnDiag = x + y === 2;
+  const isRowWin = piece === L && piece === R;
+  const isColWin = piece === U && piece === D;
+  const isDiagWin =
+    (isOnUpDiag && piece === UL && piece === DR) ||
+    (isOnDnDiag && piece === UR && piece === DL);
+  return isRowWin || isColWin || isDiagWin;
+};
+
 const svgPieces = {
   X: (
     <svg viewBox="0 0 8 8">
@@ -20,24 +40,30 @@ const Square = ({ piece, isWin }) => (
   </div>
 );
 
-export default function App() {
+const Board = ({ pieces }) => (
+  <div className="board">
+    {pieces.map((row, y) =>
+      row.map((piece, x) => (
+        <Square piece={piece} isWin={isWin(pieces, x, y)} />
+      ))
+    )}
+  </div>
+);
+
+export default function Game() {
   return (
-    <div className="App">
+    <div className="game">
       <div className="header">
         Next Player:
         <div className="icon">{svgPieces.X}</div>
       </div>
-      <div className="board">
-        <Square piece="X" isWin={true} />
-        <Square piece="O" />
-        <Square piece="X" />
-        <Square piece="O" />
-        <Square piece="X" isWin={true} />
-        <Square piece="O" />
-        <Square piece="X" />
-        <Square piece="" />
-        <Square piece="X" isWin={true} />
-      </div>
+      <Board
+        pieces={[
+          ["O", "X", "O"],
+          ["O", "X", "O"],
+          ["X", "X", "X"]
+        ]}
+      />
       <button className="newGame">New Game</button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import Game from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Game />
   </StrictMode>,
   rootElement
 );
